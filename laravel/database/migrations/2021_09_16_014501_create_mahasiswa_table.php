30efdb632cd6c4b373a5d48c3ab40f2ccad51c5b
nugroho-dev <momochi@yahoo.com>
2021-09-16 12:29:52 +0700

    api laravel

---
+++ laravel/database/migrations/2021_09_16_014501_create_mahasiswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 100);
+            $table->string('tempat_lahir', 50);
+            $table->date('tgl_lahir');
+            $table->tinyInteger('jk')->comment('1=laki-laki, 2=perempuan');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
