a7faf54da3605b422e317725d111355c2043a131
= <bbhpowpow@gmail.com>
2021-09-16 12:53:12 +0700

    UPLOAD LARAVEL

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::group(['namespace' => 'API'], function()
+{
+    /**
+     * route resource mahasiswa
+     */
+    Route::resource('/mahasiswa', MahasiswaController::class)->only([
+        'index', 'store', 'edit', 'update', 'destroy'
+    ]);
+});
